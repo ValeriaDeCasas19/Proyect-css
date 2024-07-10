f88875dbe7aed968dae7e4000217ff05b6a69748
ValeriaDeCasas19 <valeriadecasasa@gmail.com>
2024-07-09 20:44:29 -0600

    Creacion de barra de navegacion y aplicacion de fuentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import styled from "styled-components"
 import GlobalStyles from "./components/GlobalStyles"
+import Cabecera from "./components/Cabecera"
+import BarraLateral from "./components/BarraLateral"
+import Banner  from "./components/Banner"
+import banner from "./assets/banner.png"
+
 
 const FondoGradiente = styled.div`
 background: linear-gradient(172deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
 width: 100%;
 min-height: 100vh;
+`
+const AppContainer = styled.div `
+width: 1280px;
+max-width: 100%;
+margin: 0 auto;
 `
 
 function App() {
@@ -14,7 +24,12 @@ function App() {
     <>
     <FondoGradiente>
       <GlobalStyles/>
-
+      <Cabecera/>
+      <AppContainer>
+      <BarraLateral/>
+      <Banner texto ="La galeria mas completa de fotos del espacio"
+      backgroundImage={banner}/>
+      </AppContainer>
     </FondoGradiente>
     </>
   )
